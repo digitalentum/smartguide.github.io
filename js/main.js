8c3646d15a9619b975559f9ecf4eda0994c20b2c
digitalentum <digitalentum@users.noreply.github.com>
2018-02-16 05:26:47 +0100

    Initial commit.

---
+++ js/main.js
@@ -0,0 +1,46 @@
+(function () { 
+    $(document).ready(function() {
+		$('.contact-form').on('submit', function(e) {
+			var form = e.currentTarget;
+
+			if (form.checkValidity() === false) {
+				event.preventDefault();
+				event.stopPropagation();
+			} else {
+				$.ajax({
+					method: 'GET',
+					url: 'https://script.google.com/macros/s/AKfycbwRkoFDUoZAWUs2O9Aa6rfMl1bYXxPzVDlEz6mAzFt-e6xSSaQ/exec',
+					dataType: 'jsonp',
+					data: {
+						firstName: $(form).find("#firstName").val(),
+						lastName: $(form).find("#lastName").val(),
+						company: $(form).find("#company").val(),
+						email: $(form).find("#email").val(),
+						app: 'SmartManual'
+					}
+				});
+				
+				event.preventDefault();
+				event.stopPropagation();
+			}
+
+			$(form).addClass('was-validated');
+		});
+
+		window.jsonpCallback = function(data) {
+			if (data.hasOwnProperty('success') && data.success) {
+				var container = $('.contact-form-align-container');
+				container.fadeOut({
+					done: function() {
+						container.empty();
+						container.css('height', '52em');
+						container.append('<div class="text-center align-self-center">' + 
+						                 '<h2 class="text-light">Done, you are on the Early Access Member list!</h2>' + 
+										 '</div>');
+						container.fadeIn();
+					}
+				});
+			}
+		}
+	});
+})();
